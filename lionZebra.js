b9e8f0f457248d66e006c99fdbfaaed1c1ada73a
javed-27 <javedahmed70634@gmail.com>
2025-09-20 13:34:16 +0530

    Create lionZebra.js

---
+++ lionZebra.js
@@ -0,0 +1,6 @@
+const lion = 'L';
+const zebra = 'Z';
+const testCase1 = "Z Z Z";
+const testCaseToUse = testCase1;
+let shortestPath = -1;
+console.log("Input: ", testCaseToUse, "Output: ", shortestPath);
