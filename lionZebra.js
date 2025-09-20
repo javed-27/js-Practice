1c9d8956088f641bd24c3d0672277ae1265fa6b0
javed-27 <javedahmed70634@gmail.com>
2025-09-20 13:49:16 +0530

    updated code to get output for the testCase2

--- lionZebra.js
+++ lionZebra.js
@@ -1,6 +1,22 @@
 const lion = 'L';
 const zebra = 'Z';
 const testCase1 = "Z Z Z";
-const testCaseToUse = testCase1;
+const testCase2 = "L Z";
+const testCaseToUse = testCase2;
 let shortestPath = -1;
+let lionFound = 0;
+let zebraFound = 0;
+for(let index = 0; index < testCaseToUse.length ; index++) {
+    if (testCaseToUse[index] === lion ) {
+        lionFound = index;
+        console.log("lion found",index);
+        
+    }
+    if (testCaseToUse[index] === zebra ) {
+        zebraFound = index;
+        console.log("zebra found",index);
+        
+    }
+    shortestPath = (zebraFound -lionFound)-1;
+}
 console.log("Input: ", testCaseToUse, "Output: ", shortestPath);
