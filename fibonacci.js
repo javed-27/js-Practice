4f53e4efb0a003863ecf8eab78d44678db6d9a17
javed-27 <javedahmed70634@gmail.com>
2025-09-21 12:28:34 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -2,4 +2,4 @@ const numberOfTerms = 3;
 const firstNumber = 0;
 const secondNumber = 1;
 const currentNumber = firstNumber + secondNumber ;
-console.log(firstNumber , secondNumber, tempValue);
+console.log(firstNumber , secondNumber, currentNumber);
