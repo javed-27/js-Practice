fee946fd1b75865b8d85957fc71778a926f5f994
javed-27 <javedahmed70634@gmail.com>
2025-09-21 12:57:49 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,12 @@
-const numberOfTerms = 5;
+const numberOfTerms = 6;
 let firstTerm = 0;
 let secondTerm = 1;
 let currentTerm = firstTerm + secondTerm ;
 console.log(firstTerm , secondTerm, currentTerm);
+firstTerm = secondTerm ;
+secondTerm = currentTerm;
+currentTerm = firstTerm + secondTerm ;
+console.log(currentTerm);
 firstTerm = secondTerm ;
 secondTerm = currentTerm;
 currentTerm = firstTerm + secondTerm ;
