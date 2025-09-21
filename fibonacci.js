a7c9a3017efb7e4816608e92539bcb08382f5219
javed-27 <javedahmed70634@gmail.com>
2025-09-21 13:02:38 +0530

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,17 +1,11 @@
 const numberOfTerms = 6;
 let firstTerm = 0;
 let secondTerm = 1;
-let currentTerm = firstTerm + secondTerm ;
-console.log(firstTerm , secondTerm, currentTerm);
-firstTerm = secondTerm ;
-secondTerm = currentTerm;
-currentTerm = firstTerm + secondTerm ;
-console.log(currentTerm);
-firstTerm = secondTerm ;
-secondTerm = currentTerm;
-currentTerm = firstTerm + secondTerm ;
-console.log(currentTerm);
-firstTerm = secondTerm ;
-secondTerm = currentTerm;
-currentTerm = firstTerm + secondTerm ;
-console.log(currentTerm);
+let currentTerm = firstTerm + secondTerm;
+console.log(firstTerm, secondTerm, currentTerm);
+for (let range = 0; range < (numberOfTerms / 2); range++) {
+    firstTerm = secondTerm;
+    secondTerm = currentTerm;
+    currentTerm = firstTerm + secondTerm;
+    console.log(currentTerm);
+}
