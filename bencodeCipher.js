28a60764ddca09fc5aacb3eb030f2e3dea72b39b
javed-27 <javedahmed70634@gmail.com>
2025-11-03 14:20:23 +0530

    Update bencodeCipher.js

--- bencodeCipher.js
+++ bencodeCipher.js
@@ -16,6 +16,10 @@ function encode(data) {
   return "l" + encodeList(data) + "e";
 }
 
+function decode(data) {
+  return parseInt(data.slice(1, data.length -1));
+}
+
 function messageToPrint(description, data, expect, actual) {
   const isPass = expect === actual;
   const status = isPass ? "✅" : "❌";
@@ -32,6 +36,12 @@ function testEncode(description, data, expect) {
   console.log(message);
 }
 
+function testDecode(description, data, expect) {
+  const actual = decode(data);
+  const message = messageToPrint(description, data, expect, actual);
+  console.log(message);
+}
+
 function heading(text) {
 console.log(`\n ${"-".repeat(20) + text + "-".repeat(20)} \n`);
 }
@@ -40,24 +50,30 @@ function testAll() {
   testEncodeNumber();
   testEncodeString();
   testEncodeList();
+  testDecodeNumber();
+}
+
+function testDecodeNumber() {
+  heading(" decoding a number ");
+  testDecode("checking positive integer", "i123e", 123);
 }
 
 function testEncodeNumber() {
-  heading(" encoding number ");
-  testEncode("checking positive number", 123, "i123e");
-  testEncode("checking negative number", -123, "i-123e");
+  heading(" encoding a number ");
+  testEncode("checking positive integer", 123, "i123e");
+  testEncode("checking negative integer", -123, "i-123e");
   testEncode("checking number 0", 0, "i0e");
 }
 
 function testEncodeString() {
-  heading(" encoding string ");
+  heading(" encoding a string ");
   testEncode("checking hello", "hello", "5:hello");
   testEncode("checking empty string", "", "0:");
   testEncode("checking special charecter string", "special!@#$chars", "16:special!@#$chars");
 }
 
 function testEncodeList() {
-  heading(" encoding list ");
+  heading(" encoding a list ");
   testEncode("checking list", [0, "", "test"], "li0e0:4:teste");
   testEncode("checking nested list", ["apple", 123, ["banana", -5]], "l5:applei123el6:bananai-5eee");
 }
