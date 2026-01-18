805fdf37735319ac0b49a0c475c97b996a6360ce
javed-27 <javedahmed70634@gmail.com>
2026-01-18 10:50:53 +0530

    Update display_status.js

--- quiz/display_status.js
+++ quiz/display_status.js
@@ -3,9 +3,12 @@ import { green, red } from "jsr:@std/fmt/colors";
 export const display = (entries, quizData) => {
   const report = quizData.map(({ question, correct_answer }, i) => {
     const status = entries[i] ? green : red;
-    return status(`${question}\n${correct_answer}`);
+    return `${question}\n${status(correct_answer)}`;
   });
 
   console.log(report.join("\n\n"));
+  const message = entries.map((status) => status ? "🌟" : "☠️").join("");
+  const total_score = entries.reduce((sum, option) => sum + option);
+  console.log(`\n${message} ${(total_score / quizData.length) * 100}%`);
   return report.join("\n\n");
 };
